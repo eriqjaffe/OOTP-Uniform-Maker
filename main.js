5be76fc15e584823453902aa201b8a1c066a8f93
Eriq Jaffe <eriqjaffe@gmail.com>
2023-04-12 09:26:40 -0500

    removed unnecessary comment

--- main.js
+++ main.js
@@ -18,7 +18,6 @@ const hasbin = require('hasbin');
 const fontname = require('fontname')
 const { createWorker } = require('tesseract.js');
 const replaceColor = require('replace-color')
-console.log("hello")
 
 const { log } = console;
 function proxiedLog(...args) {
